cb55d77ebc05396c470c06789f0dfee65f5e302f
porkiedev <porkiedev@gmail.com>
2026-07-24 11:36:33 -0500

    Update squelch-collector git hash

--- packages/squelch-collector.nix
+++ packages/squelch-collector.nix
@@ -8,10 +8,10 @@ rustPlatform.buildRustPackage rec {
     owner = "porkiedev";
     repo = "squelch-collector";
     rev = "ba95bc7989770842e4d0f4c36e4c36f9edc830da";
-    hash = lib.fakeHash;
+    hash = "sha256-pe500uLayaIjJg0+9RpxjFXyPJcKPG7Oo6+Fwp1mT5w=";
   };
 
-  cargoHash = lib.fakeHash; # same trick -- run once, copy the real hash from the error
+  cargoHash = lib.fakeHash;
 
   meta = with lib; {
     description = "A tool for parsing, aggregating, and forwarding the output of various RF tools to a SurrealDB instance for fun and monitoring purposes";
