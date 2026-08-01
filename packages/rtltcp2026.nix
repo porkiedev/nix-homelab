81b1a80d5091251341872076cadb9cd8dfc90006
porkiedev <porkiedev@gmail.com>
2026-08-01 15:17:22 -0500

    Add rtltcp2026 package

---
+++ packages/rtltcp2026.nix
@@ -0,0 +1,49 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  rtl-sdr-blog,
+  systemd,
+}:
+with import <nixpkgs> {
+  overlays = [
+    (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
+  ];
+};
+let
+  rustPlatform = makeRustPlatform {
+    cargo = rust-bin.stable.latest.minimal;
+    rustc = rust-bin.stable.latest.minimal;
+  };
+in
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "rtltcp2026";
+  version = "0.10.1";
+
+  src = fetchFromGitHub {
+    owner = "dlasher";
+    repo = "rtltcp2026";
+    rev = "v${version}";
+    hash = lib.fakeHash;
+  };
+
+  cargoHash = lib.fakeHash;
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [
+    rtl-sdr-blog
+    systemd
+  ];
+
+  meta = with lib; {
+    description = "A rust implementation of rtl-tcp with better buffering and support for systemd socket activation";
+    homepage = "https://github.com/dlasher/rtltcp2026";
+    license = with lib.licenses; [ mit asl20 ];
+    mainProgram = "rtltcp2026";
+    platforms = lib.platforms.linux;
+  };
+})
